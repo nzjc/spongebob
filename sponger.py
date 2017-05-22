c61ff50151501d679eee1bcba9a7078c7645e547
nzjc <r@dical.org>
2017-05-22 11:41:05 +0100

    Update sponger.py
    Added some code to ensure we don't go over 140 chars, breaking the cycle :)

--- sponger.py
+++ sponger.py
@@ -19,22 +19,39 @@ def sponge(stringo):
                         dringo = dringo + dringo.join(char.upper())
                 else:
                         dringo = dringo + dringo.join(char.lower())
-
         return dringo
 
-#Don't tweet yourself
 def removeName(stringo):
         stringo = stringo.replace("@spongebob_it","")
         return stringo
 
+def lessThan140(text,id):
+        front = id + text
+        if len(front)+1 < 122:
+                return text
+        else:
+                x = len(id)
+                y = len(text)
+                z = 121 - x
+                text = text[:z]
+        return text
+
+
 def tweetIt(id, text):
         #Send a tweet
-        text = text.replace("  ", " ")
+        text = text.replace("  ", "")
+        text = lessThan140(text,id)
         with open("that-us.jpg", "rb") as imagefile:
                 imagedata = imagefile.read()
         t_upload = Twitter(domain='upload.twitter.com',auth=OAuth(token, token_secret, consumer_key, consumer_secret))
         id_img1 = t_upload.media.upload(media=imagedata)["media_id_string"]
 
+#       print("Total Length: ")
+#       print(len('@'+id+text+id_img1))
+#       print(len(id)+1)
+#       print(len(text))
+#       print(len(id_img1))
+#       print(text)
         t.statuses.update(status="@"+id+" "+text, media_ids=id_img1)
 
 
@@ -43,9 +60,10 @@ startUp = t.statuses.mentions_timeline(count=1)
 for tweet in startUp:
         text = removeName(tweet['text'])
         id = tweet['user']['screen_name']
-        tweetIt(id, sponge(text))
+        #tweetIt(id, sponge(text))
         if tweet['id'] > highest_id:
                 highest_id = tweet['id']
+
 while True:
         newTweets = t.statuses.mentions_timeline(count=5,since_id=highest_id)
         for tweets in newTweets:
